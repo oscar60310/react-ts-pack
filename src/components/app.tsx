89d48692e04a1c4b0d403a14fcb12be1cdf985b1
Ivan Tsai <oscar60310@gmail.com>
2019-10-03 23:09:30 +0800

    add async hook and test case

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { TestComponent } from './test-component';
 const AppComponent = () => {
-  return <div>React TS Pack Work!</div>;
+  return (
+    <div>
+      React TS Pack Work!
+      <TestComponent />
+    </div>
+  );
 };
 export default hot(AppComponent);
---
+++ src/components/test-component.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export const TestComponent = () => {
+  return <div>Test</div>;
+};
